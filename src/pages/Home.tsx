7f54b9ee3c84fcfa105d2df35ff555cc1f246e5b
chtah <chawakorn_ha@hotmail.com>
2023-10-29 22:06:53 +0700

    add lock icon

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,25 @@
 import classes from './Home.module.css'
 import cleverse_card from '../assets/course_card_cleverse.png'
 import reactHandsOn_card from '../assets/course_card_react-hands-on.png'
+import lock_Icon from '../assets/lock_icon.svg'
 import { Link } from 'react-router-dom'
+import { useAuth } from '../providers/AuthProvider'
 
 const Home = () => {
+  const { isLoggedIn } = useAuth()
   return (
     <div>
       <div className={classes.container}>
         <Link to={`/course/cleverse`}>
           <div className={classes.card}>
             <img className={classes.image} src={cleverse_card} />
+            <p className={!isLoggedIn ? classes.ButtonloginRequired : classes.lockIconDeactive}>Login required</p>
             <div className={classes.cardText}>
               <div className={classes.cardTextUp}>
                 <p className={classes.projectName}>Cleverse</p>
                 <p className={classes.description}>Live Records</p>
                 <p className={classes.description}>Update 27-Oct-2023</p>
+                <img className={!isLoggedIn ? classes.lockIconActive : classes.lockIconDeactive} src={lock_Icon} />
               </div>
             </div>
           </div>
